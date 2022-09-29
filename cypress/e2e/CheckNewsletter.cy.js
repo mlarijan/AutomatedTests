8f37ea6236a92a7133f8968aafab7c3073e3a6cd
Mariusz Dygant <dygantmariusz@gmail.com>
2022-09-29 17:05:35 +0200

    add TC02- already registered email

--- cypress/e2e/CheckNewsletter.cy.js
+++ cypress/e2e/CheckNewsletter.cy.js
@@ -1,4 +1,4 @@
-describe('CheckNewsletter.js', () => {
+describe('CheckNewsletter.cy.js', () => {
     it('TC01. Check if the newsletter is working properly when we input valid, not registered email adress', () => {
         cy.visit('http://automationpractice.com/index.php')
         cy.get('[name="email"]').type('validNewEmail@gmail.com')
@@ -6,4 +6,13 @@ describe('CheckNewsletter.js', () => {
         cy.get('.alert-success').contains("Newsletter : You have successfully subscribed to this newsletter.")
 
     });
+})
+
+describe('CheckNewsletter.cy.js', () => {
+  it('TC02. Check if the newsletter is working properly when we input valid, already registered email adress', () => {
+      cy.visit('http://automationpractice.com/index.php')
+      cy.get('[name="email"]').type('validNewEmail@gmail.com')
+      cy.get('[name="submitNewsletter"]').click()
+      cy.get('.alert-danger').contains (" Newsletter : This email address is already registered.")
+  });
 })
