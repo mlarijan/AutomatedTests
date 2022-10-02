82349d024d7891d7049281948cbb89dccae03c6a
Mariusz Dygant <dygantmariusz@gmail.com>
2022-10-02 18:23:33 +0200

    Added several test cases for comprehensive logging testing

---
+++ cypress/e2e/CreateAccount.cy.js
@@ -0,0 +1,9 @@
+describe('Creating an account', () => {
+    it.only ('TC01. Creating an account with valid email adress', () => {
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage').click()
+        cy.get("#email_create").type('validEmail@gmail.com')
+        cy.get('#SubmitCreate').click() 
+
+    })
+})
