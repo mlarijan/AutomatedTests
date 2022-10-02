4cce8bbb1bfc0d82cea2ce206e30d14c0e1fbf17
Mariusz Dygant <dygantmariusz@gmail.com>
2022-10-02 18:27:52 +0200

    Added several test cases for comprehensive logging testing

--- cypress/e2e/CreateAccount.cy.js
+++ cypress/e2e/CreateAccount.cy.js
@@ -2,7 +2,7 @@ describe('Creating an account', () => {
     it.only ('TC01. Creating an account with valid email adress', () => {
         cy.visit('http://automationpractice.com/index.php')
         cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage').click()
-        cy.get("#email_create").type('validEmail@gmail.com')
+        cy.get("#email_create").type('dygantmariusz2@gmail.com')
         cy.get('#SubmitCreate').click() 
 
     })
--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,9 +1,53 @@
 describe('Testing Login', () => {
-    it('TC01. Check if we can log in with valid user name and password. Account exists.', () => {
+    it('TC01. Check if we can log in with valid email and valid password. The account exists. Check if we enter "my account" page.', () => {
         cy.visit('http://automationpractice.com/index.php')
-        cy.get('[name="email"]').type('validNewEmail@gmail.com')
-        cy.get('[name="submitNewsletter"]').click()
-        cy.get('.alert-success').contains("Newsletter : You have successfully subscribed to this newsletter.")
+        cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage')
+        cy.get('@SignInBtnMainPage').click()
+        cy.get("#email").type('dygantmariusz2@gmail.com')
+        cy.get("#passwd").type('haslo123')
+        cy.get('#SubmitLogin').click()
+        cy.url().should('include', '/index.php?controller=my-account')
+    })
 
-    });
+    it("TC02. Check if we can log in with valid email and valid password. The account doesn't exist. Check error message.", () => {
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage')
+        cy.get('@SignInBtnMainPage').click()
+        cy.get("#email").type('validEmail@gmail.com')
+        cy.get("#passwd").type('validPassword')
+        cy.get('#SubmitLogin').click()
+        cy.get("li").parents("ol").find("li").should('contain','Authentication failed.')
+     })
+
+    it("TC03. Check if we can log in with invalid email and valid password. Check error message.", () => {
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage')
+        cy.get('@SignInBtnMainPage').click()
+        cy.get("#email").type('invalidEmailgmail.com')
+        cy.get("#passwd").type('validPassword')
+        cy.get('#SubmitLogin').click()
+        cy.get("li").parents("ol").find("li").should('contain','Invalid email address.')
+    })
+
+
+    it("TC04. Check if we can log in with valid email and invalid password. Check error message.", () => {
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage')
+        cy.get('@SignInBtnMainPage').click()
+        cy.get("#email").type('validEmail@gmail.com')
+        cy.get("#passwd").type('iP')
+        cy.get('#SubmitLogin').click()
+        cy.get("li").parents("ol").find("li").should('contain','Invalid password.')
+    })
+
+    it("TC05. Check if we can log in with invalid email and invalid password. Check if there are two error messages. Check error messages.", () => {
+        cy.visit('http://automationpractice.com/index.php')
+        cy.get('[title="Log in to your customer account"]').as('SignInBtnMainPage')
+        cy.get('@SignInBtnMainPage').click()
+        cy.get("#email").type('invalidEmailgmail.com')
+        cy.get("#passwd").type('iP')
+        cy.get('#SubmitLogin').click()
+        cy.get("li").parent("ol").find("li").eq(0).should('contain','Invalid email address.')
+        cy.get("li").parent("ol").find("li").eq(1).should('contain','Invalid password.')
+    })
 })
